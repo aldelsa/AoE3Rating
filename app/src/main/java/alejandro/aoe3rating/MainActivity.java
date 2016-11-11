9cc1d8f096ffb1acb6cfc6ce25db57840955c9c9
Alejandro Delgado <Alex@MacBook-Pro-de-Alejandro.local>
2016-11-11 21:50:30 +0100

    First commit

---
+++ app/src/main/java/alejandro/aoe3rating/MainActivity.java
@@ -0,0 +1,64 @@
+package alejandro.aoe3rating;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.StrictMode;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.*;
+import org.apache.commons.io.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView texto = (TextView)findViewById(R.id.textView3);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy =
+                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        try {
+            String urlString = "http://www.agecommunity.com/query/query.aspx";
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+
+            InputStream stream = conn.getInputStream();
+            String line;
+            try {
+
+                br = new BufferedReader(new InputStreamReader(is));
+                while ((line = stream.readzz()) != null) {
+                    sb.append(line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
