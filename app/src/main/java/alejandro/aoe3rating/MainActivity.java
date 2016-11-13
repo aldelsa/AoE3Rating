a189f3485aeb0002c124bfec2ce718948f20eca3
aldelsa <aldelsa1@inf.upv.es>
2016-11-13 12:22:04 +0100

    Check several errors

--- app/src/main/java/alejandro/aoe3rating/MainActivity.java
+++ app/src/main/java/alejandro/aoe3rating/MainActivity.java
@@ -2,43 +2,21 @@ package alejandro.aoe3rating;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.widget.Button;
 import android.widget.TextView;
-import android.os.StrictMode;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.*;
-import org.apache.commons.io.*;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView texto = (TextView)findViewById(R.id.textView3);
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy =
-                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
-        try {
-            String urlString = "http://www.agecommunity.com/query/query.aspx";
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            conn.connect();
-
-            InputStream stream = conn.getInputStream();
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        texto = (TextView)findViewById(R.id.textView3);
     }
+
+    public void cambiarMensaje(View v){
+        texto.setText("adios");
+    }
+
 }
